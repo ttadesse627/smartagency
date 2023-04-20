8260c7e7c99ad8fdd25c417a6a0c53cd9bb9eaae
mahlet <mahlet@gmail.com>
2023-04-20 14:58:42 +0300

    intial commit

---
+++ AppDiv.CRVS.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿
+using AppDiv.CRVS.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace AppDiv.CRVS.Domain
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        public string UserGroupId { get; set; }
+        public string PersonalInfoId { get;set;}
+        public virtual PersonalInfo PersonalInfo { get; set; }
+    }
+}
