96e69acbac16e2ae6cc7fe834294a8086cf7b50f
Tesfaye Tadesse <ttadesse627@gmail.com>
2023-05-01 09:47:52 +0300

    Feature: Get Lookup Categories Added.

---
+++ src/AppDiv.SmartAgency.API/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+
+
+using AppDiv.SmartAgency.Application.Common;
+using AppDiv.SmartAgency.Application.Contracts.DTOs.CategoryDTOs;
+using AppDiv.SmartAgency.Application.Features.Query.Attachments;
+using AppDiv.SmartAgency.Application.Features.Query.Categories;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppDiv.SmartAgency.API.Controllers;
+
+[ApiController]
+[Route("api/category")]
+public class CategoryController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public CategoryController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpGet("get-all")]
+    public async Task<ActionResult<CategoryResponseDTO>> GetAllCategories()
+    {
+        return Ok(await _mediator.Send(new GetAllCategories()));
+    }
+}
