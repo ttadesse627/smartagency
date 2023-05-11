ae6ad55faf6ee192dcc7b17e4274230f5f8ee28c
Tesfaye Tadesse <ttadesse627@gmail.com>
2023-05-11 11:35:25 +0300

    Fix: Default value for success message modified

--- src/AppDiv.SmartAgency.Application/Common/ServiceResponse.cs
+++ src/AppDiv.SmartAgency.Application/Common/ServiceResponse.cs
@@ -4,7 +4,7 @@ namespace AppDiv.SmartAgency.Application.Common;
 public class ServiceResponse<T>
 {
     public T? Data { get; set; }
-    public bool Success { get; set; } = true;
+    public bool Success { get; set; } = false;
     public string Message { get; set; } = string.Empty;
     public List<string>? Errors { get; set; }
 
