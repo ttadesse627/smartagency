04ba4a775c3faa83148c946bc0104dd23424cfda
Tesfaye Tadesse <ttadesse627@gmail.com>
2023-05-11 12:38:26 +0300

    Fix: type conflicts

--- src/AppDiv.SmartAgency.Application/Features/Command/Update/Partners/EditPartnerCommand.cs
+++ src/AppDiv.SmartAgency.Application/Features/Command/Update/Partners/EditPartnerCommand.cs
@@ -1,5 +1,5 @@
 
-using AppDiv.SmartAgency.Application.Contracts.DTOs.AddressDTOs;
+using AppDiv.SmartAgency.Application.Contracts.DTOs.Common;
 using AppDiv.SmartAgency.Application.Contracts.DTOs.PartnersDTOs;
 using AppDiv.SmartAgency.Application.Contracts.Request.Common;
 using AppDiv.SmartAgency.Application.Interfaces.Persistence;
