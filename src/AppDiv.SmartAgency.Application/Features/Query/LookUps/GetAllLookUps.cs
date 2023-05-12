cb88e93c2dae5af15f23e58c9918180d8d7c4394
Tesfaye Tadesse <ttadesse627@gmail.com>
2023-05-12 22:58:12 +0300

    Feature: Parameters added to support pagination

--- src/AppDiv.SmartAgency.Application/Features/Query/LookUps/GetAllLookUps.cs
+++ src/AppDiv.SmartAgency.Application/Features/Query/LookUps/GetAllLookUps.cs
@@ -1,10 +1,22 @@
 
+using AppDiv.SmartAgency.Application.Common;
 using AppDiv.SmartAgency.Application.Contracts.DTOs.LookUpDTOs;
 using MediatR;
 
 namespace AppDiv.SmartAgency.Application.Features.Query.LookUps
 {
-    public class GetAllLookUps: IRequest<List<LookUpResponseDTO>>
+    public record GetAllLookUps : IRequest<PaginatedList<LookUpResponseDTO>>
     {
+        public string SearchTerm { get; set; }
+        public string[] ColumnNames { get; set; } = new string[]{"Value"};
+        public int PageNumber { get; init; }
+        public int PageSize { get; init; }
+        public GetAllLookUps(string searchTerm = "", int pageNumber = 1, int pageSize = 10)
+        {
+            SearchTerm = searchTerm;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+
     }
 }
--- src/AppDiv.SmartAgency.Application/Features/Query/LookUps/GetAllLookUpsHandler.cs
+++ src/AppDiv.SmartAgency.Application/Features/Query/LookUps/GetAllLookUpsHandler.cs
@@ -2,26 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AppDiv.SmartAgency.Application.Common;
 using AppDiv.SmartAgency.Application.Contracts.DTOs.LookUpDTOs;
 using AppDiv.SmartAgency.Application.Interfaces.Persistence;
 using AppDiv.SmartAgency.Application.Mapper;
+using AppDiv.SmartAgency.Domain.Entities;
+using AppDiv.SmartAgency.Domain.Entities.Orders;
 using MediatR;
 
 namespace AppDiv.SmartAgency.Application.Features.Query.LookUps
 {
-    public class GetAllLookUpsHandler: IRequestHandler<GetAllLookUps, List<LookUpResponseDTO>>
-{
-    private readonly ILookUpRepository _lookUpRepository;
+    public class GetAllLookUpsHandler : IRequestHandler<GetAllLookUps, PaginatedList<LookUpResponseDTO>>
+    {
+        // private readonly ISmartAgencyDbContext _context;
+        // private readonly IMapper _mapper;
+        private readonly ILookUpRepository _lookUpRepository;
 
         public GetAllLookUpsHandler(ILookUpRepository lookUpRepository)
         {
             _lookUpRepository = lookUpRepository;
         }
-        public async Task<List<LookUpResponseDTO>> Handle(GetAllLookUps request, CancellationToken cancellationToken)
+        public async Task<PaginatedList<LookUpResponseDTO>> Handle(GetAllLookUps request, CancellationToken cancellationToken)
         {
-            var lookUpList = await _lookUpRepository.GetAllAsync();
-            var lookUpResponse = CustomMapper.Mapper.Map<List<LookUpResponseDTO>>(lookUpList);
-            return lookUpResponse;
+
+            var lookUpList = await _lookUpRepository.GetAllWithSearchAsync(request.ColumnNames, request.SearchTerm, "Category");
+
+            var paginatedListLookUp = new PaginatedList<LookUp>((IReadOnlyCollection<LookUp>)lookUpList, lookUpList.Count(), request.PageNumber, request.PageSize);
+            var paginatedList = await PaginatedList<LookUp>.CreateAsync(lookUpList, request.PageNumber, request.PageSize);
+            var paginatedListResp = CustomMapper.Mapper.Map<PaginatedList<LookUpResponseDTO>>(paginatedList);
+
+            return paginatedListResp;
         }
-}
+    }
 }
