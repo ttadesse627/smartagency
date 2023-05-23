9b2ec1faf1824b6c280777ce76bbd1d5eb286a2c
Tesfaye Tadesse <ttadesse627@gmail.com>
2023-05-23 13:20:10 +0300

    fix: modified

--- src/AppDiv.SmartAgency.Application/Interfaces/Persistence/IApplicantRepository.cs
+++ src/AppDiv.SmartAgency.Application/Interfaces/Persistence/IApplicantRepository.cs
@@ -1,7 +1,6 @@
 
 
 using AppDiv.SmartAgency.Application.Common;
-using AppDiv.SmartAgency.Application.Features.Applicants.Command.Update;
 using AppDiv.SmartAgency.Application.Interfaces.Persistence.Base;
 using AppDiv.SmartAgency.Domain.Entities.Applicants;
 
@@ -16,5 +15,4 @@ public interface IApplicantRepository : IBaseRepository<Applicant>
     public Task<ServiceResponse<Int32>> SaveDbUpdateAsync();
     public Task<ServiceResponse<Applicant>> GetApplicantByPassportNumber(string passportNumber);
     public Task<int> AddApplicantAsync(Applicant applicant);
-    public Task<Applicant> GetApplicantByIdWithAsync(Guid id);
 }
