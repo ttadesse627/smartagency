b9efc2fa072417bad94f4ab8a46978064e1aa498
Tesfaye Tadesse <ttadesse627@gmail.com>
2023-05-23 12:41:01 +0300

    Feature: #998, #999

--- src/AppDiv.SmartAgency.Application/Features/Orders/Query/GetAllOrders.cs
+++ src/AppDiv.SmartAgency.Application/Features/Orders/Query/GetAllOrders.cs
@@ -1,36 +1,52 @@
-using AppDiv.SmartAgency.Application.Common;
-using AppDiv.SmartAgency.Application.Contracts.DTOs.OrderDTOs;
+using AppDiv.SmartAgency.Application.Contracts.DTOs.OrderDTOs.GetOrdersDTOs;
 using AppDiv.SmartAgency.Application.Interfaces.Persistence;
 using AppDiv.SmartAgency.Application.Mapper;
-using AppDiv.SmartAgency.Domain.Entities.Orders;
+using AppDiv.SmartAgency.Utility.Contracts;
 using MediatR;
 
 namespace AppDiv.SmartAgency.Application.Features.Orders.Query;
 
-public class GetAllOrders : IRequest<List<OrderResponseDTO>>
+public record GetAllOrders : IRequest<SearchModel<GetOrdersResponseDTO>>
 {
     public int PageNumber { get; set; }
     public int PageSize { get; set; }
+    public string SearchTerm { get; set; } = string.Empty;
+    public string OrderBy { get; set; } = string.Empty;
+    public SortingDirection SortingDirection { get; set; } = SortingDirection.Ascending;
+    public GetAllOrders(int pageNumber, int pageSize, string searchTerm, string orderBy, SortingDirection sortingDirection)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        SearchTerm = searchTerm;
+        OrderBy = orderBy;
+        SortingDirection = sortingDirection;
+    }
+
 }
 
-public class GetAllOrdersHandler : IRequestHandler<GetAllOrders, List<OrderResponseDTO>>
+public class GetAllOrdersHandler : IRequestHandler<GetAllOrders, SearchModel<GetOrdersResponseDTO>>
 {
-    
+
     private readonly IOrderRepository _orderRepository;
 
     public GetAllOrdersHandler(IOrderRepository orderRepository)
     {
         _orderRepository = orderRepository;
     }
-    public async Task<List<OrderResponseDTO>> Handle(GetAllOrders request, CancellationToken cancellationToken)
+    public async Task<SearchModel<GetOrdersResponseDTO>> Handle(GetAllOrders request, CancellationToken cancellationToken)
     {
-        var orderList = await _orderRepository.GetAllWithAsync
+        var eagerLoadedProperties = new string[]
+                                    {
+                                        "Priority","Sponsor", "OrderCriteria",
+                                        "OrderCriteria.Salary",  "OrderCriteria.JobTitle",
+                                        "Employee","Payment", "Employee", "Partner"
+                                    };
+        var orderList = await _orderRepository.GetAllWithPredicateSearchAsync
                         (
-                            "Partner", "PortOfArrival", "Priority",
-                            "VisaType", "Employee", "VisaFile",
-                            "OrderCriteria", "OrderPayment", "OrderSponsor", "VisaFile.FileCollectionAttachment");
-        var paginatedList = new PaginatedList<Order>((IReadOnlyCollection<Order>)orderList, orderList.Count(), request.PageNumber, request.PageSize);
-        var orderResponse = CustomMapper.Mapper.Map<List<OrderResponseDTO>>(orderList);
+                            request.PageNumber, request.PageSize, request.SearchTerm, request.OrderBy,
+                            request.SortingDirection, order => order.IsDeleted == false, eagerLoadedProperties
+                        );
+        var orderResponse = CustomMapper.Mapper.Map<SearchModel<GetOrdersResponseDTO>>(orderList);
         return orderResponse;
     }
 }
