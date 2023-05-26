f2f04f25918e543e188b45729f81a89ef624f45e
mahlet <mahlet@gmail.com>
2023-05-26 14:50:27 +0300

    Feature:#747 view single looup item added

--- src/AppDiv.SmartAgency.Application/Features/LookUps/Query/GetLookUpByIdQuery.cs
+++ src/AppDiv.SmartAgency.Application/Features/LookUps/Query/GetLookUpByIdQuery.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AppDiv.SmartAgency.Application.Contracts.DTOs.LookUpDTOs;
 using AppDiv.SmartAgency.Domain.Entities;
 using AppDiv.SmartAgency.Utility.Contracts;
 using MediatR;
 
 namespace AppDiv.SmartAgency.Application.Features.LookUps.Query
 {
-    public record GetLookUpByIdQuery: IRequest<LookUp>
+    public record GetLookUpByIdQuery: IRequest<LookUpResponseDTO>
     {
         public Guid Id { get; private set; }
 
