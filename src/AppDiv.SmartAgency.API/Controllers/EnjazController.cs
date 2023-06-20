52cb965ed3839a84c741e92af73174f225e8fbc8
Tesfaye Tadesse <ttadesse627@gmail.com>
2023-06-20 09:38:11 +0300

    Feature: Get All Enjazs

--- src/AppDiv.SmartAgency.API/Controllers/EnjazController.cs
+++ src/AppDiv.SmartAgency.API/Controllers/EnjazController.cs
@@ -2,16 +2,14 @@
 
 
 
-using AppDiv.SmartAgency.Application.Contracts.DTOs.DepositDTOs;
 using AppDiv.SmartAgency.Application.Contracts.Request.Enjazs;
 using AppDiv.SmartAgency.Application.Features.Enjazs.Command.Create;
-using AppDiv.SmartAgency.Application.Features.Deposits.Command.Delete;
-using AppDiv.SmartAgency.Application.Features.Deposits.Command.Update;
 using AppDiv.SmartAgency.Application.Features.Deposits.Query;
 using AppDiv.SmartAgency.Application.Common;
 using AppDiv.SmartAgency.Utility.Contracts;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using AppDiv.SmartAgency.Application.Contracts.DTOs.EnjazDTOs;
 
 namespace AppDiv.SmartAgency.API.Controllers
 {
@@ -33,12 +31,12 @@ namespace AppDiv.SmartAgency.API.Controllers
             return Ok(response);
         }
 
-        // [HttpGet("get-all-deposits")]
-        // public async Task<ActionResult<DepositResponseDTO>> GetAllDeposits(int pageNumber = 1, int pageSize = 10, string? searchTerm = "", string? orderBy = null, SortingDirection sortingDirection = SortingDirection.Ascending)
-        // {
-        //     return Ok(await _mediator.Send(new GetAllDepositQuery(pageNumber, pageSize, searchTerm, orderBy, sortingDirection)));
+        [HttpGet("get-all")]
+        public async Task<ActionResult<SearchModel<EnjazResponseDTO>>> GetAllEnjazs(int pageNumber = 1, int pageSize = 10, string? searchTerm = "", string? orderBy = null, SortingDirection sortingDirection = SortingDirection.Ascending)
+        {
+            return Ok(await _mediator.Send(new GetAllEnjazsQuery(pageNumber, pageSize, searchTerm, orderBy, sortingDirection)));
 
-        // }
+        }
 
 
         // [HttpGet("{id}")]
---
+++ src/AppDiv.SmartAgency.Application/Features/Enjaz/Query/GetAllEnjazsQuery.cs
@@ -0,0 +1,72 @@
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppDiv.SmartAgency.Application.Contracts.DTOs.EnjazDTOs;
+using AppDiv.SmartAgency.Application.Interfaces.Persistence;
+using AppDiv.SmartAgency.Application.Interfaces.Persistence.Base;
+using AppDiv.SmartAgency.Application.Mapper;
+using AppDiv.SmartAgency.Utility.Contracts;
+using MediatR;
+
+namespace AppDiv.SmartAgency.Application.Features.Deposits.Query
+{
+    public class GetAllEnjazsQuery : IRequest<SearchModel<EnjazResponseDTO>>
+    {
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string SearchTerm { get; set; } = string.Empty;
+        public string OrderBy { get; set; } = string.Empty;
+        public SortingDirection SortingDirection { get; set; } = SortingDirection.Ascending;
+        public GetAllEnjazsQuery(int pageNumber, int pageSize, string searchTerm, string orderBy, SortingDirection sortingDirection)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            SearchTerm = searchTerm;
+            OrderBy = orderBy;
+            SortingDirection = sortingDirection;
+        }
+
+    }
+
+    public class GetAllEnjazsQueryHandler : IRequestHandler<GetAllEnjazsQuery, SearchModel<EnjazResponseDTO>>
+    {
+        private readonly IEnjazRepository _enjazRepository;
+        private readonly ISmartAgencyDbContext _dbContext;
+
+        public GetAllEnjazsQueryHandler(IEnjazRepository enjazRepository, ISmartAgencyDbContext dbContext)
+        {
+            _enjazRepository = enjazRepository;
+            _dbContext = dbContext;
+        }
+        public async Task<SearchModel<EnjazResponseDTO>> Handle(GetAllEnjazsQuery request, CancellationToken cancellationToken)
+        {
+            var enjazsList = await _enjazRepository.GetAllWithSearchAsync(request.PageNumber, request.PageSize, request.SearchTerm, request.OrderBy, request.SortingDirection, null, "Order", "Order.Employee");
+            var enjazResponse = new SearchModel<EnjazResponseDTO>();
+            if (enjazsList.Items.Count() > 0 || enjazsList != null)
+            {
+                foreach (var enjaz in enjazsList.Items)
+                {
+                    var enjazResp = new EnjazResponseDTO
+                    {
+                        EnjazNumber = enjaz.ApplicationNumber,
+                        OrderNumber = enjaz.Order.OrderNumber,
+                        VisaNumber = enjaz.Order.VisaNumber,
+                        OrderId = enjaz.OrderId.ToString(),
+                        PassportNumber = enjaz.Order.Employee.PassportNumber,
+                        FirstName = enjaz.Order.Employee.FirstName,
+                        MiddleName = enjaz.Order.Employee.MiddleName,
+                        LastName = enjaz.Order.Employee.LastName
+                    };
+                    enjazResponse.Items.ToList().Add(enjazResp);
+                }
+            }
+            return enjazResponse;
+
+            // return (List<Customer>)await _customerQueryRepository.GetAllAsync();
+        }
+    }
+}
