480dcf3d6da267e90686a84720382201d0a06ee3
Tesfaye Tadesse <ttadesse627@gmail.com>
2023-06-30 17:41:17 +0300

    FIx: changed process response

--- src/AppDiv.SmartAgency.Application/Features/Processes/Query/GetProcessDefinitionsQuery.cs
+++ src/AppDiv.SmartAgency.Application/Features/Processes/Query/GetProcessDefinitionsQuery.cs
@@ -2,39 +2,33 @@
 
 
 using AppDiv.SmartAgency.Application.Common;
+using AppDiv.SmartAgency.Application.Contracts.DTOs.Common;
 using AppDiv.SmartAgency.Application.Contracts.DTOs.ProcessDTOs;
 using AppDiv.SmartAgency.Application.Interfaces.Persistence;
 using AppDiv.SmartAgency.Application.Mapper;
 using MediatR;
 
 namespace AppDiv.SmartAgency.Application.Features.Processes.Query;
-public record GetProcessDefinitionsQuery(Guid id) : IRequest<ServiceResponse<List<GetProcessDefinitionResponseDTO>>> { }
-public class GetProcessDefinitionsQueryHandler : IRequestHandler<GetProcessDefinitionsQuery, ServiceResponse<List<GetProcessDefinitionResponseDTO>>>
+public record GetProcessDefinitionsQuery(Guid id) : IRequest<ResponseContainerDTO<List<GetProcessDefinitionResponseDTO>>> { }
+public class GetProcessDefinitionsQueryHandler : IRequestHandler<GetProcessDefinitionsQuery, ResponseContainerDTO<List<GetProcessDefinitionResponseDTO>>>
 {
     private readonly IProcessDefinitionRepository _definitionRepository;
     public GetProcessDefinitionsQueryHandler(IProcessDefinitionRepository definitionRepository)
     {
         _definitionRepository = definitionRepository;
     }
-    public async Task<ServiceResponse<List<GetProcessDefinitionResponseDTO>>> Handle(GetProcessDefinitionsQuery query, CancellationToken cancellationToken)
+    public async Task<ResponseContainerDTO<List<GetProcessDefinitionResponseDTO>>> Handle(GetProcessDefinitionsQuery query, CancellationToken cancellationToken)
     {
         var explicitLoadedProperties = new string[]
         {
             "ApplicantProcesses", "ApplicantProcesses.Applicant",
             "ApplicantProcesses.Applicant.Order", "ApplicantProcesses.Applicant.Order.Sponsor",
         };
-        var response = new ServiceResponse<List<GetProcessDefinitionResponseDTO>>();
+        var response = new ResponseContainerDTO<List<GetProcessDefinitionResponseDTO>>();
         var processes = await _definitionRepository.GetAllWithPredicateAsync(pd => pd.ProcessId == query.id, explicitLoadedProperties);
         if (processes.Count() > 0)
         {
-            response.Data = CustomMapper.Mapper.Map<List<GetProcessDefinitionResponseDTO>>(processes);
-            response.Success = true;
-            response.Message = $"{response.Data.Count} record/s is/are fetched!";
-        }
-        else
-        {
-            response.Message = "No Record Found";
-            response.Success = false;
+            response.Items = CustomMapper.Mapper.Map<List<GetProcessDefinitionResponseDTO>>(processes);
         }
         return response;
     }
--- src/AppDiv.SmartAgency.Application/Features/Processes/Query/GetProcessQuery.cs
+++ src/AppDiv.SmartAgency.Application/Features/Processes/Query/GetProcessQuery.cs
@@ -1,14 +1,15 @@
 
 
 using AppDiv.SmartAgency.Application.Common;
+using AppDiv.SmartAgency.Application.Contracts.DTOs.Common;
 using AppDiv.SmartAgency.Application.Contracts.DTOs.ProcessDTOs;
 using AppDiv.SmartAgency.Application.Interfaces.Persistence;
 using AppDiv.SmartAgency.Application.Mapper;
 using MediatR;
 
 namespace AppDiv.SmartAgency.Application.Features.Processes.Query;
-public class GetProcessQuery : IRequest<ServiceResponse<List<GetProcessResponseDTO>>> { }
-public class GetProcessQueryHandler : IRequestHandler<GetProcessQuery, ServiceResponse<List<GetProcessResponseDTO>>>
+public class GetProcessQuery : IRequest<ResponseContainerDTO<List<GetProcessResponseDTO>>> { }
+public class GetProcessQueryHandler : IRequestHandler<GetProcessQuery, ResponseContainerDTO<List<GetProcessResponseDTO>>>
 {
     private readonly IProcessRepository _processRepository;
     private readonly IApplicantProcessRepository _applicantRepository;
@@ -17,25 +18,15 @@ public class GetProcessQueryHandler : IRequestHandler<GetProcessQuery, ServiceRe
         _processRepository = processRepository;
         _applicantRepository = applicantRepository;
     }
-    public async Task<ServiceResponse<List<GetProcessResponseDTO>>> Handle(GetProcessQuery query, CancellationToken cancellationToken)
+    public async Task<ResponseContainerDTO<List<GetProcessResponseDTO>>> Handle(GetProcessQuery query, CancellationToken cancellationToken)
     {
-        var response = new ServiceResponse<List<GetProcessResponseDTO>>();
+        var response = new ResponseContainerDTO<List<GetProcessResponseDTO>>();
         var excLoadedProps = new string[] { "Country", "ProcessDefinitions" };
         var processes = await _processRepository.GetAllWithAsync(excLoadedProps);
         if (processes.Count() > 0)
         {
-            // foreach (var process in processes)
-            // {
-            //     var pdResponse = CustomMapper.Mapper.Map<GetPDResponseDTO>(process.ProcessDefinitions);
-            // }
-            response.Success = true;
-            response.Data = CustomMapper.Mapper.Map<List<GetProcessResponseDTO>>(processes);
-            response.Message = $"{response.Data.Count} record/s is/are fetched!";
-        }
-        else
-        {
-            response.Message = "No Record Found";
-            response.Success = false;
+            response.Items = CustomMapper.Mapper.Map<List<GetProcessResponseDTO>>(processes);
+
         }
         return response;
     }
