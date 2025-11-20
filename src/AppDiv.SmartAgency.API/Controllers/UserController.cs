fb8b7f2efdfb5503f1a33394a999691635d3661f
ttadesse627 <ttadesse627@gmail.com>
2025-11-20 11:24:13 +0300

    Split to Auth Module

--- src/AppDiv.SmartAgency.API/Controllers/UserController.cs
+++ src/AppDiv.SmartAgency.API/Controllers/UserController.cs
@@ -9,11 +9,6 @@ using AppDiv.SmartAgency.Application.Features.Users.Command.Create;
 using AppDiv.SmartAgency.Application.Features.Users.Query;
 using AppDiv.SmartAgency.Utility.Contracts;
 using Microsoft.AspNetCore.Mvc;
-using AppDiv.SmartAgency.Application.Features.Auth.Login;
-using AppDiv.SmartAgency.Application.Features.Auth.ForgotPassword;
-using AppDiv.SmartAgency.Application.Features.Auth.ResetPassword;
-using AppDiv.SmartAgency.Application.Features.Auth.ChangePassword;
-using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace AppDiv.SmartAgency.API.Controllers
 {
@@ -108,36 +103,6 @@ namespace AppDiv.SmartAgency.API.Controllers
             {
                 return BadRequest(exp.Message);
             }
-        }
-
-        [HttpPost("Login")]
-        public async Task<IActionResult> Login([FromBody] AuthCommand command)
-        {
-            return Ok(await Mediator.Send(command));
-        }
-
-        [HttpPost("forgot-password")]
-        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordCommand command)
-        {
-            return Ok(await Mediator.Send(command));
-        }
-
-        [HttpPost("resetPassword")]
-        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordCommand command)
-        {
-            return Ok(await Mediator.Send(command));
-        }
-
-        [HttpPost("change-password")]
-        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordCommand command)
-        {
-            return Ok(await Mediator.Send(command));
-        }
-
-        [HttpPost("Logout")]
-        public async Task<IActionResult> Logout()
-        {
-            return Ok(await Mediator.Send(new LogoutCommand()));
         }
 
     }
